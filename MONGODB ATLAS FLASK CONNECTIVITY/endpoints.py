c095a6d814c632eff537a0bbc4847bbd58c8656f
sinegalatha <89790198+sinegalatha@users.noreply.github.com>
2022-10-30 11:22:29 +0530

    Update endpoints.py

--- MONGODB ATLAS FLASK CONNECTIVITY/endpoints.py
+++ MONGODB ATLAS FLASK CONNECTIVITY/endpoints.py
@@ -1,98 +1,100 @@
-from http import client
 import logging
 from flask_pymongo import pymongo
 from flask import jsonify, request
-import urllib.parse
 
-con_string="mongodb+srv://Sinegalatha:murugavinayaga@cluster0.7tzvukd.mongodb.net/?retryWrites=true&w=majority"
-client=pymongo.MongoClient(con_string)
-db=client.get_database('Mongo')
-user_collection=pymongo.collection.Collection(db,'collection1')
+con_string = "mongodb+srv://ramya:muruga@cluster0.x57r05g.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(con_string)
+db = client.get_database('testdb')
+user_collection = pymongo.collection.Collection(db, 'usercollection')
 print("Mongo db connected successfully")
-def project_api_route(endpoints):
-    @endpoints.route('/hello',methods=['GET'])
+
+
+def project_api_routes(endpoints):
+    @endpoints.route('/hello', methods=['GET'])
     def hello():
-        res='Hello'
-        #print(res)
-        print(user_collection)
-        return user_collection
-    @endpoints.route('/register_user',methods=['POST'])
+        res = 'Hello World'
+        print('Hello World')
+        return res
+
+    @endpoints.route('/register-user', methods=['POST'])
     def register_user():
-        resp={}
+        resp = {}
         try:
-            req_body=request.json
+            req_body = request.json
             user_collection.insert_one(req_body)
             print("user data successfully added")
-            status={
-                "statusCode":"200",
-                "statusMessage":"User data successfully stored"
+            status1 = {
+                "statusCode": "200",
+                "statusMessage": "User data successfully stored"
             }
         except Exception as e:
-            status={
-                "statusCode":"400",
-                "statusMessage":str(e)
+            print(e)
+            status1 = {
+                "statusCode": "400",
+                "statusMessage": str(e)
             }
-        resp["status"]=status
+        resp["status"] = status1
         return resp
-    @endpoints.route('/read_users',methods=['GET'])
+
+    @endpoints.route('/read_users', methods=['GET'])
     def read_users():
-        resp={}
+        resp = {}
         try:
-            users=user_collection.find(())
+            users = user_collection.find({})
             print(users)
-            users=list(users)
-            status={
-                "statusCode":"200",
-                "statusMessage":"User data successfully retrieved"
+            users = list(users)
+            print("user data retrieved successfully")
+            status = {
+                "statusCode": "200",
+                "statusMessage": "User data successfully retrieved"
             }
-            output=[{'Name':user['name'],'Email':user['email']} for user in users]
-            resp['data']=output
-        except:
-            status={
-                "statusCode":"400",
-                "statusMessage":str(e)
+            output = [{'Name': user['name'], 'Email': user['email']} for user in users]
+            resp['data'] = output
+        except Exception as e:
+            print(e)
+            status = {
+                "statusCode": "400",
+                "statusMessage": str(e)
             }
-            resp["status"]=status
-            return resp
-    @endpoints.route('/update-users',methods=['PUT'])
+        resp["status"] = status
+        return resp
+
+    @endpoints.route('/update-users', methods=['PUT'])
     def update_users():
-        resp={}
+        resp = {}
         try:
-            req_body=request.json
-            user_collection.update_one({"id":req_body['id']},{"$set":req_body['updated_user_body']})
+            req_body = request.json
+            user_collection.update_one({"id": req_body['id']}, {"$set": req_body['updated_user_body']})
             print("user updated successfully")
-            status={
-                "statusCode":"200",
-                "statusMessage":"User data successfully updated"
+            status = {
+                "statusCode": "200",
+                "statusMessage": "User data successfully updated"
             }
-        except:
-            status={
-                "statusCode":"400",
-                "statusMessage":str(e)
+        except Exception as e:
+            status = {
+                "statusCode": "400",
+                "statusMessage": str(e)
             }
-            resp["status"]=status
-            return resp
-    @endpoints.route('/delete',methods=['DELETE'])
+        resp["status"] = status
+        return resp
+
+    @endpoints.route('/delete', methods=['DELETE'])
     def delete():
-        resp={}
+        resp = {}
         try:
-            delete_id=request.args.get('delete_id')
-            user_collection.delete_one({"id":delete_id})
-            status={
-                "statusCode":"200",
-                "statusMessage":"User data successfully deleted"
+            delete_id = request.args.get('delete_id')
+            user_collection.delete_one({"id": delete_id})
+            status = {
+                "statusCode": "200",
+                "statusMessage": "User data successfully deleted"
             }
-        
-        except:
-            status={
-                "statusCode":"400",
-                "statusMessage":str(e)
+
+        except Exception as e:
+            status = {
+                "statusCode": "400",
+                "statusMessage": str(e)
             }
-            resp["status"]=status
-            return resp
+        resp["status"] = status
+        return resp
+
     return endpoints
-
-
-
-        
-
